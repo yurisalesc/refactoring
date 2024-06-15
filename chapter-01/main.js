f176d9cce347390a213e8239a179c85b67110178
Yuri Sales <yuri.sales@protonmail.com>
2024-06-15 10:32:34 -0300

    fix: change function format declaration to usd

--- chapter-01/main.js
+++ chapter-01/main.js
@@ -1,3 +1,13 @@
+function usd(aNumber) {
+  return new Intl.NumberFormat(
+    "en-US",
+    {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2
+    }
+  ).format(aNumber);
+}
 
 function playFor(aPerformance) {
   return plays[aPerformance.playID];
@@ -36,22 +46,13 @@ function statement(invoice) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat(
-    "en-US",
-    {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2
-    }
-  ).format;
-
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 }
