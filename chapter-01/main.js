19265bff51191297fb02100a5c20939cfe814f63
Yuri Sales <yuri.sales@protonmail.com>
2024-06-15 10:34:09 -0300

    fix: move calc to usd function

--- chapter-01/main.js
+++ chapter-01/main.js
@@ -6,7 +6,7 @@ function usd(aNumber) {
       currency: "USD",
       minimumFractionDigits: 2
     }
-  ).format(aNumber);
+  ).format(aNumber/100);
 }
 
 function playFor(aPerformance) {
@@ -48,11 +48,11 @@ function statement(invoice) {
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `Amount owed is ${usd(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 }
