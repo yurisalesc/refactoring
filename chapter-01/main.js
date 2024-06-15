2c53be945c56d4686b5ab18e41934e63f0301d79
Yuri Sales <yuri.sales@protonmail.com>
2024-06-15 10:49:31 -0300

    fix: extract totalVolumeCredits function from statement, and replace it with query

--- chapter-01/main.js
+++ chapter-01/main.js
@@ -42,18 +42,23 @@ function volumeCreditsFor(aPerformance) {
   return result;
 }
 
+function totalVolumesCredits(invoice) {
+  let result = 0;
+  for (let perf of invoice.performances) {
+    result += volumeCreditsFor(perf);
+  }
+  return result;
+}
+
 function statement(invoice) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
-
   result += `Amount owed is ${usd(totalAmount)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+  result += `You earned ${totalVolumesCredits(invoice)} credits\n`;
   return result;
 }
 
