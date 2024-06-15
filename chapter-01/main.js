f2583682b26d105f811f9f6bf12bd5de20e0f2cd
Yuri Sales <yuri.sales@protonmail.com>
2024-06-15 10:08:47 -0300

    fix: remove play temp variable to inline call to playFor()

--- chapter-01/main.js
+++ chapter-01/main.js
@@ -39,18 +39,17 @@ function statement(invoice, plays) {
   ).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
     let thisAmount = 0;
 
-    thisAmount = amountFor(perf, play);
+    thisAmount = amountFor(perf, playFor(perf));
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
     // print line for this order
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
   }
 
